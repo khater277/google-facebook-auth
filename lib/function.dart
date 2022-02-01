13f6d2f4fdd97fce0f18d17221e7353d29a3a0cb
khater277 <66753803+khater277@users.noreply.github.com>
2022-02-02 01:49:46 +0200

    google-facebook-auth

--- lib/function.dart
+++ lib/function.dart
@@ -27,31 +27,24 @@ String formatName(String name){
 }
 
 Future<UserCredential> signInWithGoogle() async {
-  // Trigger the authentication flow
   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  // Obtain the auth details from the request
   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
 
-  // Create a new credential
   final credential = GoogleAuthProvider.credential(
     accessToken: googleAuth?.accessToken,
     idToken: googleAuth?.idToken,
   );
 
-  // Once signed in, return the UserCredential
   return await FirebaseAuth.instance.signInWithCredential(credential);
 }
 
 Future<UserCredential> signInWithFacebook() async {
-  // Trigger the sign-in flow
   final LoginResult loginResult = await FacebookAuth.instance.login();
 
-  // Create a credential from the access token
   final OAuthCredential facebookAuthCredential =
   FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
-  // Once signed in, return the UserCredential
   return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
 }
 
